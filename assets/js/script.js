c78e5f86875e7412e0399f701baeb6a35234be3a
Adilene Bejines <adi3511@hotmail.com>
2021-11-18 20:24:38 -0700

    updated task array

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ var tasksCompletedEl = document.querySelector("#tasks-completed");
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+var tasks = [];
+
 var taskFormHandler = function(event) {
     
     event.preventDefault();
@@ -32,10 +34,14 @@ var taskFormHandler = function(event) {
    } else {
        var taskDataObj = {
            name: taskNameInput,
-           type: taskTypeInput
+           type: taskTypeInput,
+           status: "to do"
         };
 
         createTaskEl(taskDataObj);
+        
+    console.log(taskDataObj);
+    console.log(taskDataObj.status);
     }
 };
      
@@ -53,6 +59,9 @@ var createTaskEl = function (taskDataObj) {
         var taskActionsEl = createTaskActions(taskIdCounter);
         listItemEl.appendChild(taskActionsEl);
         tasksToDoEl.appendChild(listItemEl);        
+
+        taskDataObj.id = taskIdCounter;
+        tasks.push(taskDataObj);
 
         taskIdCounter++;
 };
@@ -98,6 +107,14 @@ var completeEditTask = function(taskName, taskType, taskId) {
         taskSelected.querySelector("h3.task-name").textContent = taskName;
         taskSelected.querySelector("span.task-type").textContent = taskType;
 
+        //loop through tasks array and task object with new content
+        for(var i = 0;i < tasks.length;i++) {
+            if (tasks[i].id === parseInt(taskId)) {
+                tasks[i].name = taskName;
+                tasks[i].type = taskType;
+            }
+        };
+
         alert("Task Updated!");
 
         formEl.removeAttribute("data-task-id");
@@ -135,6 +152,12 @@ var taskStatusChangeHandler = function(event) {
     } else if (statusValue === "completed") {
         tasksCompletedEl.appendChild(taskSelected);
     }
+    for (var i = 0;i < tasks.length;i++) {
+        if (tasks[i].id === parseInt(taskId)) {
+            tasks[i].status = statusValue;
+        console.log(tasks);
+        }
+    }
 };
     
 var editTask = function(taskId) {
@@ -162,6 +185,16 @@ var deleteTask = function(taskId) {
 
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
     taskSelected.remove();
+
+    var updatedTaskArr = [];
+    //loop through current tasks
+    for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id !== parseInt(taskId)) {
+            updatedTaskArr.push(tasks[i]);
+        }
+    }
+    //reassign tasks array to be the same as updatedTaskArr
+    tasks = updatedTaskArr;
 };
 pageContentEl.addEventListener("click", taskButtonHandler);
 formEl.addEventListener("submit", taskFormHandler);
